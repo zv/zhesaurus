78d87fe0ac0fc41ae4868ca877bdc4c4b4513356
Zephyr Pellerin <zv@nxvr.org>
2016-01-15 18:05:41 -0800

    Added in skeleton of key press handling code

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,17 @@
 
 extern crate gtk;
 extern crate gdk;
+#[macro_use] extern crate log;
 
-use std::cell::Cell;
 use std::env;
 use std::error::Error;
-use std::fs::File;
-use std::fs;
-use std::io::prelude::*;
-use std::path::Path;
-use std::rc::Rc;
-use std::sync::{Arc, Mutex};
+use log::*;
 
 use gtk::traits::*;
 use gtk::{signal, widgets};
 use gtk::signal::Inhibit;
 use gtk::signal::TreeViewSignals;
 use gtk::Orientation::{Vertical};
-
-use gdk::enums::key;
-use gdk::enums::modifier_type;
 
 #[cfg(feature="search_entry")]
 fn get_entry_field() -> gtk::SearchEntry {
@@ -33,34 +25,40 @@ fn get_entry_field() -> gtk::Entry {
     gtk::Entry::new().unwrap_or_else(|| panic!("Unable to instantiate GTK::Entry!"))
 }
 
+fn key_press_handler(key_widget: gtk::Widget, key: &gdk::EventKey) -> Inhibit {
+   let keyval = key.keyval as i32;
+    println!("key pressed: {}", keyval);
+    return Inhibit(false)
+}
+
 fn main() {
     gtk::init().unwrap_or_else(|_| panic!("Failed to initialize GTK."));
 
-    // debug!("Major: {}, Minor: {}", gtk::get_major_version(), gtk::get_minor_version());
+    debug!("Major: {}, Minor: {}", gtk::get_major_version(), gtk::get_minor_version());
 
     let (window, entry) = {
         let window = gtk::widgets::Window::new(gtk::WindowType::Toplevel).unwrap();
         let container = gtk::widgets::Box::new(Vertical, 0).unwrap();
         let entry = get_entry_field();
-        container.add(&entry);
-        container.set_visible(true);
         window.add(&container);
+        container.add(&entry);
         window.connect_delete_event(|_, _| {
            gtk::main_quit();
            Inhibit(false)
         });
-        // Setup our window properties
+        // Setup our window pro
         window.set_window_position(gtk::WindowPosition::Center);
-        window.set_border_width(0);
+        window.set_border_width(5);
         window.set_decorated(false);
         window.show_all();
         (window, entry)
     };
 
+    window.connect_key_press_event(key_press_handler);
 
     // env_logger::init().unwrap_or_else(|x| panic!("Error initializing logger: {}", x));
 
     gtk::main();
 
-    return;
+    ()
 }
